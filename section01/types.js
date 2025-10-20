d03c9e9a47c86768a68fb2dbfb2891d4876f76d8
Yunho Park <qkrdbsgh0111@gmail.com>
2025-10-20 22:45:33 +0900

    자료형

---
+++ section01/types.js
@@ -0,0 +1,30 @@
+// 1. Number Type
+let num1 = 29;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN; // Not a Number (수치 연산 실패했을 때)
+
+
+// 2. String Type
+let myLocation = "서대문구";
+let myName = "박윤호";
+let introduce = myLocation + myName; // 서대문구박윤호
+
+let introduceText = `${myName}은 ${myLocation}에 거주하고 있습니다.`; // Template Literal
+
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+
+// 4. Null Type
+let empty = null;
+
+
+// 5. Undefined Type
+let none;
