efb049aab93a3704125fdb3871d1a6c5c987388d
Yunho Park <qkrdbsgh0111@gmail.com>
2025-10-30 22:48:52 +0900

    React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Main from "./components/Main.jsx";
 
+// 루트(Root) 컴포넌트 -> Header, Main, Footer 컴포넌트를 자식으로 둔 계층 구조
 function App() {
     return (
         <>
+            <Header/> {/* 부모 컴포넌트 */}
+            <Main/>
             <h1>안녕 리액트!</h1>
+            <Footer/>
         </>
     );
 }
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// 자식 컴포넌트 (반드시 첫 글자가 대문자여야함!!)
+const Header = () => {
+    return (
+        <header>
+            <h1>header</h1>
+        </header>
+    );
+};
+
+export default Header;
