fa4beedb9982f6cae92f30728033349fc42c8428
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-02 15:06:38 +0900

    이벤트 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,23 @@
 const Button = ({ text, color = "black", children }) => { // 구조분해할당으로 color 값에 기본값 정의
     console.log(`text=${text}, color=${color}`);
-    return <button style={{ color }}>{text} - {children}</button>;
+
+    const onButtonClick = (event) => {
+        /*
+        통합된 이벤트 객체를 제공해준다 (Synthetic Base Event, 합성 이벤트)
+            -> 브라우저 별 스펙이 달라 발생하는 문제 (Cross Browsing Issue)
+            -> 합성 이벤트 객체(=통일된 규칙)를 통해 해결 가능
+         */
+        console.log(event);
+        console.log(text);
+    };
+
+    return <button
+        // 이벤트 핸들러
+        onClick={onButtonClick}
+        onMouseEnter={onButtonClick}
+        style={{ color }}>
+        {text} - {children}
+    </button>;
 };
 
 export default Button;
