3048722ff1115d7d922fdca772ce03241931c977
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-07 23:17:50 +0900

    React.memo()와 컴포넌트 렌더링 최적화

--- section07/src/components/Header.jsx
+++ section07/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import "./Header.css";
+import { memo } from "react";
 
-export default () => {
+const Header = () => {
     return (
         <div className={"Header"}>
             <h3>오늘은 📆</h3>
             <h1>{new Date().toDateString()}</h1>
         </div>
     );
-}
+};
+
+export default memo(Header);
--- section07/src/components/TodoItem.jsx
+++ section07/src/components/TodoItem.jsx
@@ -1,13 +1,9 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
-export default ({ id, isDone, content, date, onUpdateTodo, onDeleteTodo }) => {
-    const onChangeCheckBox = () => {
-        onUpdateTodo(id);
-    };
-
-    const onClickDeletedButton = () => {
-        onDeleteTodo(id);
-    };
+const TodoItem = ({ id, isDone, content, date, onUpdateTodo, onDeleteTodo }) => {
+    const onChangeCheckBox = () => onUpdateTodo(id);
+    const onClickDeletedButton = () => onDeleteTodo(id);
 
     return (
         <div className={"TodoItem"}>
@@ -17,4 +13,24 @@ export default ({ id, isDone, content, date, onUpdateTodo, onDeleteTodo }) => {
             <button onClick={onClickDeletedButton}>삭제</button>
         </div>
     );
-}
+};
+
+// 고차 컴포넌트(HOC)
+export default memo(TodoItem, (prevProps, nextProps) => {
+    // 반환값에 따라 props의 변경 여부를 판단
+    // true: props 변경되지 않음 -> 리렌더링 X
+    // false: props 변경됨 -> 리렌더링 O
+
+    const keys = ["id", "isDone", "content", "date"];
+    return keys.some((key) => prevProps[key] !== nextProps[key]);
+});
+
+/*
+React.memo()는 기본적으로 인수로 받은 컴포넌트의 props간 얕은 비교를 수행하여 메모이제이션 여부를 결정함.
+따라서, 객체 타입의 값(ex. onUpdateTodo, onDeleteTodo)을 매번 새롭게 전달이 될 때마다 다른 주소값을 가지게 됨
+props가 변경이 되었다고 판단하여 메모이제이션이 수행되지 않고 리렌더링 발생!!
+
+[해결 방안]
+1. useCallback() 리액트 훅 이용
+2. 두 번째 인수로 콜백 함수를 전달하여 최적화 커스터마이징
+ */
