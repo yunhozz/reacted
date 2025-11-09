d9131cfcb9ba567a79c3bd400c1fd927d716f2b5
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-09 19:10:48 +0900

    React Router DOM 의존성 설정

--- section08/src/main.jsx
+++ section08/src/main.jsx
@@ -1,7 +1,11 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <App/>
+    // React App의 모든 컴포넌트들이 페이지 라우팅과 관련된 모든 데이터들을 공급받을 수 있음
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
