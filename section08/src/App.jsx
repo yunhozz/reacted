59329b9fb179273fdf539c0b33b868cfccc40c46
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-09 20:01:02 +0900

    기본 정적 파일(assets, public) 추가 및 경로 설정

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import Diary from "./pages/Diary.jsx";
+import Home from "./pages/Home.jsx";
+import New from "./pages/New.jsx";
+import Notfound from "./pages/Notfound.jsx";
+import { getEmotionImage } from "./util/get-emotion-image.js";
 
 export default () => {
+    const nav = useNavigate();
+
+    const onClickButton = () => nav("/new");
+
     return (
-        <div className={"App"}>App</div>
+        <>
+            <div>
+                <img src={getEmotionImage(1)}/>
+                <img src={getEmotionImage(2)}/>
+                <img src={getEmotionImage(3)}/>
+                <img src={getEmotionImage(4)}/>
+                <img src={getEmotionImage(5)}/>
+            </div>
+            <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/new"}>New</Link>
+                <Link to={"/diary"}>Diary</Link>
+            </div>
+            <button onClick={onClickButton}>New 페이지로 이동</button>
+            <Routes className={"App"}>
+                <Route path={"/"} element={<Home/>}></Route>
+                <Route path={"/new"} element={<New/>}></Route>
+                <Route path={"/diary/:id"} element={<Diary/>}></Route>
+                <Route path={"*"} element={<Notfound/>}></Route>
+            </Routes>
+        </>
     );
 }
--- section08/src/main.jsx
+++ section08/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
+    // React Router DOM : Client Side Rendering(CSR) 방식으로 호출
     // React App의 모든 컴포넌트들이 페이지 라우팅과 관련된 모든 데이터들을 공급받을 수 있음
     <BrowserRouter>
         <App/>
